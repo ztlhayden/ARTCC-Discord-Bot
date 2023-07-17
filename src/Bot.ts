68d70dce33860de522e0cafc84f85cf59944d967
ztlhayden <haydenyounglex@hotmail.com>
2023-07-17 12:48:28 -0400

    create client

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,20 @@
 import { Client } from 'discord.js'
+require('dotenv').config()
+import ready from './listeners/ready'
 
+//bring in the tokens
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+
+//debuging log
 console.log('Bot is starting...')
 
+//create the client object
 const client = new Client({
   intents: [],
 })
 
-console.log(client)
+//register the ready listener with the client
+ready(client)
+
+//log in
+client.login(DISCORD_TOKEN)
